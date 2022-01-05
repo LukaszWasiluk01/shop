293d1d5e1427874aab39b4be17d5f09ca19b9fb3
Łukasz Wasiluk <lukasz.wasiluk01@gmail.com>
2022-01-05 19:30:54 +0100

    Install Coverage and add simple test to Category class

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Category
 
 # Create your tests here.
+
+class TestCategoriesModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Category.objects.create(name='toys', slug='toys')
+    
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertEqual(str(data), 'toys')
