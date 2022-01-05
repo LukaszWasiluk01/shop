8e10e6406dc206887ad3d6dee21530e7b194b84c
Łukasz Wasiluk <lukasz.wasiluk01@gmail.com>
2022-01-05 19:37:57 +0100

    Create ProductListView and urls, templates, styles for it

--- store/views.py
+++ store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product, Category
 
 # Create your views here.
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = "store/products/list.html"
+    queryset = Product.objects.all()
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        return context
