f72a5a2fb813abf221fb32842336d56e773b9841
Łukasz Wasiluk <lukasz.wasiluk01@gmail.com>
2022-01-10 21:03:38 +0100

    Create search and filters functionality but it collides with each other and pagination.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Product, Category
@@ -7,14 +8,36 @@ class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
     template_name = "store/products/list.html"
-    queryset = Product.objects.filter(available=True)
     paginate_by = 6
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
         return context
-    
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(available=True)
+
+        selectors = {
+            'title' : self.request.GET.get('title'),
+            'category' : self.request.GET.get('category'),
+            'min_price' : self.request.GET.get('min_price'),
+            'max_price' : self.request.GET.get('max_price'),
+        }
+        
+        if selectors['title']:
+            queryset = queryset.filter(name__icontains=selectors['title'])
+        if selectors['category'] and selectors['category'] != "Categories":
+            queryset = queryset.filter(category__name=selectors['category'])
+        if selectors['min_price'] and selectors['max_price']:
+            queryset = queryset.filter(price__range=[selectors['min_price'],selectors['max_price']])
+        else:
+            if selectors['min_price']:
+                queryset = queryset.filter(price__gte=selectors['min_price'])
+            elif selectors['max_price']:
+                queryset = queryset.filter(price__lte=selectors['max_price'])
+        return queryset
+
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
