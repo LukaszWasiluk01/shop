aaeaf4e59de50e84166d7c10ea83eb26be9d9d9e
Łukasz Wasiluk <lukasz.wasiluk01@gmail.com>
2022-04-20 21:56:33 +0200

    Fix cart

--- store/urls.py
+++ store/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name="index"),
-    path('<slug:slug>/', views.ProductListView.as_view(), name="index_filtered"),
-    path('<int:id>/<slug:slug>/', views.product_detail, name="product_detail"),
     path('cart/', views.cart_detail, name='cart_detail'),
     path('cart/add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('cart/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name="product_detail"),
+    path('', views.ProductListView.as_view(), name="index"),
+    path('<slug:slug>/', views.ProductListView.as_view(), name="index_filtered"),
 ]
